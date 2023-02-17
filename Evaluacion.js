18a12ea8c3fefc2af777687c83b1661434879fa9
GilbihannysR <annysmaria23@gmail.com>
2023-02-17 10:09:02 -0400

    Programa que determina tu etapa de vida segun tu edad

---
+++ Evaluacion.js
@@ -0,0 +1,14 @@
+let edad ;
+    edad= 80 ;
+
+    console.log(edad);
+
+    if (edad <=12) {
+        console.log("Aun eres un Niño(a)");
+      } else if (edad <= 18) {
+        console.log("Eres Adolescente");
+      } else if (edad <= 59) {
+        console.log("Eres todo un Adulto");
+      } else {
+        console.log("Ya eres un Adulto Mayor");
+      }
